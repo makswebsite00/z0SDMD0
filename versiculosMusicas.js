14020c18c8538fb23d8f53bf4a309ed7e02464a3
makswebsite00 <qojavbot06@gmail.com>
2026-01-11 09:24:29 -0300

    Add files via upload

---
+++ versiculosMusicas.js
@@ -0,0 +1,62 @@
+export const versiculosMusicas = [
+    {
+        texto: "Não andeis ansiosos por coisa alguma; antes em tudo sejam os vossos pedidos conhecidos diante de Deus pela oração e súplica com ações de graças.",
+        referencia: "Filipenses 4:6"
+    },
+    {
+        texto: "Lançando sobre ele toda a vossa ansiedade, porque ele tem cuidado de vós.",
+        referencia: "1 Pedro 5:7"
+    },
+    {
+        texto: "Deixo-vos a paz, a minha paz vos dou; não vo-la dou como o mundo a dá. Não se turbe o vosso coração, nem se atemorize.",
+        referencia: "João 14:27"
+    },
+    {
+        texto: "Busquei ao Senhor, e ele me respondeu; livrou-me de todos os meus temores.",
+        referencia: "Salmos 34:4"
+    },
+    {
+        texto: "Quando a ansiedade prendia o meu coração, o teu consolo trouxe alívio à minha alma.",
+        referencia: "Salmos 94:19"
+    },
+    {
+        texto: "Não temas, porque eu sou contigo; não te assombres, porque eu sou teu Deus; eu te fortaleço, e te ajudo, e te sustento com a destra da minha justiça.",
+        referencia: "Isaías 41:10"
+    },
+    {
+        texto: "O Senhor é o meu pastor, nada me faltará. Deitar-me faz em verdes pastos, guia-me mansamente a águas tranquilas.",
+        referencia: "Salmos 23:1-2"
+    },
+    {
+        texto: "Por isso vos digo: Não andeis ansiosos quanto à vossa vida, pelo que haveis de comer ou pelo que haveis de beber; nem quanto ao vosso corpo, pelo que haveis de vestir.",
+        referencia: "Mateus 6:25"
+    },
+    {
+        texto: "O próprio Senhor da paz lhes dê a paz em todo o tempo e de todas as formas. O Senhor seja com todos vocês.",
+        referencia: "2 Tessalonicenses 3:16"
+    },
+    {
+        texto: "Vinde a mim, todos os que estais cansados e oprimidos, e eu vos aliviarei.",
+        referencia: "Mateus 11:28"
+    },
+    {
+        texto: "A quietude e a confiança serão a vossa força.",
+        referencia: "Isaías 30:15"
+    },
+    {
+        texto: "Sei que a bondade e a fidelidade me acompanharão todos os dias da minha vida.",
+        referencia: "Salmos 23:6"
+    },
+    {
+        texto: "O Senhor é o meu auxílio; não temerei. O que me pode fazer o homem?",
+        referencia: "Hebreus 13:6"
+    },
+    {
+        texto: "Espera no Senhor, anima-te, e ele fortalecerá o teu coração; espera, pois, no Senhor.",
+        referencia: "Salmos 27:14"
+    },
+    {
+        texto: "Pois Deus não nos deu espírito de covardia, mas de poder, de amor e de equilíbrio.",
+        referencia: "2 Timóteo 1:7"
+    }
+];
